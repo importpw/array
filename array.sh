db705d0497cd635d73507ae46c62b9a2c6369225
Nathan Rajlich <nathan@tootallnate.net>
2018-06-26 14:52:45 -0700

    Use `printf` for `array_join`
    So there's no trailing newline in the output

--- array.sh
+++ array.sh
@@ -1,2 +1,2 @@
 # https://stackoverflow.com/a/17841619/376773
-array_join() { local IFS="$1"; shift; echo "$*"; }
+array_join() { local IFS="$1"; shift; printf "%s" "$*"; }
--- test.sh
+++ test.sh
@@ -5,4 +5,7 @@ import "import.pw/assert@2.1.1"
 source ./array.sh
 
 assert_equal "$(array_join , foo)" foo
+assert_equal "$(array_join , foo | wc -c)" 3
+
 assert_equal "$(array_join , foo bar)" foo,bar
+assert_equal "$(array_join , foo bar | wc -c)" 7
