e2f9b20bf0836fe97bf777e1338fc981124e58bd
Nathan Rajlich <nathan@tootallnate.net>
2018-07-06 20:52:25 -0700

    Add `array_split`

--- array.sh
+++ array.sh
@@ -1,2 +1,4 @@
 # https://stackoverflow.com/a/17841619/376773
 array_join() { local IFS="$1"; shift; printf "%s" "$*"; }
+
+array_split() { IFS= read -d"$1" -r "$2" || [ -n "${!2}" ]; }
--- test.sh
+++ test.sh
@@ -1,11 +1,26 @@
-#!/bin/sh
+#!/bin/sh -e
 eval "`curl -fsSL import.pw`"
 import "import.pw/assert@2.1.1"
-
 source ./array.sh
 
+
+# array_join
 assert_equal "$(array_join , foo)" foo
 assert_equal "$(array_join , foo | wc -c)" 3
 
 assert_equal "$(array_join , foo bar)" foo,bar
 assert_equal "$(array_join , foo bar | wc -c)" 7
+
+
+# array_split
+i=0
+printf "foo,bar,baz" | while array_split , part; do
+  if [ $i -eq 0 ]; then
+    assert_equal "${part}" foo
+  elif [ $i -eq 1 ]; then
+    assert_equal "${part}" bar
+  elif [ $i -eq 2 ]; then
+    assert_equal "${part}" baz
+  fi
+  i=$(( $i + 1 ))
+done
